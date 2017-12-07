86fcda5ec4220f10e0a15efc7bf4811ab43b9947
Raphael Hehl <support@technohehl.de>
2017-12-07 23:42:07 +0100

    added "No password"

--- seite2.php
+++ seite2.php
@@ -10,7 +10,9 @@ $passwort = $_POST['passwort'];
 
 $pass = password_hash($passwort, PASSWORD_DEFAULT);
 
-if (password_verify($passwort, $pass)) {
+if ($passwort == NULL) {
+    echo "No password";
+} elseif (password_verify($passwort, $pass)) {
     echo "Success";
 } else {
     echo "Failed";
